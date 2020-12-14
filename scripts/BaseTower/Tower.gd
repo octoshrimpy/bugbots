594125b47f5aa7be1aee54711fa6f4af7a7cc4a4
Rocco Nicholls <rocco11nicholls@gmail.com>
2020-12-13 19:49:12 -0700

    Fixed bug

--- scripts/BaseTower/Tower.gd
+++ scripts/BaseTower/Tower.gd
@@ -17,7 +17,7 @@ func _init():
   bullet_scene = preload("res://scenes/Bullet_1.tscn")
   fire_rate = 10
   fire_speed = 150
-  rotation_speed = 5  # between 1 and 10
+  rotation_speed = 3 # between 1 and 10
   precision =  5
   gun = $Gun
 
@@ -29,7 +29,7 @@ func _process(delta):
     var lerp_speed = float(rotation_speed) * (precision * 0.01)
     gun.rotation = lerp_angle(gun.rotation, goto_rotation, lerp_speed)
 
-    var rotation_difference = abs(gun.rotation - goto_rotation)
+    var rotation_difference = fmod(abs(gun.rotation - goto_rotation), TAU)
     #TODO: add check for targets, otherwise this fires like my piano
 
     if rotation_difference < 0.1:
@@ -55,7 +55,7 @@ func debug():
   var target_location = get_target_xy()
   var mouse_loc = get_global_mouse_position()
   var goto_rotation = (target_location - gun.global_position).normalized().angle()
-  var lerp_speed = float(rotation_speed) * (precision * 0.01)
+  var rotation_difference = fmod(abs(gun.rotation - goto_rotation), TAU)
 
   print(
     "\n Mouse.x: " + str(mouse_loc.x) +
@@ -63,5 +63,6 @@ func debug():
     "\n target_within_range: " + str(target_within_range(target_location)) +
     "\n gun.rotation: " + str(gun.rotation) +
     "\n goto_rotation: " + str(goto_rotation) +
+    "\n rotation_difference: " + str(rotation_difference) +
     "\n can_shoot: " + str(gun.can_shoot)
   )
